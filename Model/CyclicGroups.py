f90f839a840b8dbf2101761f559f4f2713787327
James <birdbrain987@gmail.com>
2020-03-29 18:31:53 +0100

    Minor code cleanup

--- Model/CyclicGroups.py
+++ Model/CyclicGroups.py
@@ -1,16 +1,16 @@
 import logging
-from Model.Groups import Group
 from Model.FinGroups import FinGroup
 from Model.FinGroups import FinGroupElem
 
 cyc_logger = logging.getLogger('cyc_logger')
 cyc_logger.info('Cyc logger created')
 
+
 class CycGroup(FinGroup):
     cyc_logger.info('Initiating CycGroup class')
 
     def __init__(self, order):
-        assert(isinstance(order,int) and order > 1)
+        assert(isinstance(order, int) and order > 1)
 
         """
         Attributes:
@@ -27,7 +27,7 @@ class CycGroup(FinGroup):
         self.elements = tuple(CycGroupElem(g, self.order, associated_group=self) for g in element_list)
         cyc_logger.debug('self.elements created')
 
-        FinGroup.__init__(self, self.elements, associated_group = self, type = 'Cyclic', group_description = 'Cyclic order %d' % order)
+        FinGroup.__init__(self, self.elements, associated_group=self, type = 'Cyclic', group_description = 'Cyclic order %d' % order)
 
         cyc_logger.info('Symmetric group of order %d created' % order)
 
@@ -67,31 +67,31 @@ class CycGroupElem(FinGroupElem):
 
     def __init__(self, ZnInt, group_order, associated_group = None):
         cyc_logger.debug('Creating instance of CycGroupElem')
-        assert(isinstance(ZnInt,int))
+        assert(isinstance(ZnInt, int))
         assert(ZnInt >= 0)
 
         try:
             assert(group_order > ZnInt)
         except AssertionError:
             cyc_logger.warning(' %d is too large and  will be reduced modulo %d, resulting in %d.'
-                               %(ZnInt, group_order, ZnInt % group_order))
+                               % (ZnInt, group_order, ZnInt % group_order))
             ZnInt = ZnInt % group_order
 
-        FinGroupElem.__init__(self, associated_group = associated_group)
+        FinGroupElem.__init__(self, associated_group=associated_group)
 
         self.group_order = group_order
-        cyc_logger.debug('Group is order %d' %group_order)
+        cyc_logger.debug('Group is order %d' % group_order)
 
         self._number = ZnInt
 
-        self.display = "%s (mod %s)" %(ZnInt,group_order)
+        self.display = "%s (mod %s)" % (ZnInt, group_order)
         self._element_holder = ZnInt
 
         self.group_type = 'Cyclic'
     cyc_logger.info('CycGroupElem class defined')
 
     def __mul__(self, second):
-        cyc_logger.debug('1st element is %s, 2nd element is %s' %(str(self.display), str(second.display)))
+        cyc_logger.debug('1st element is %s, 2nd element is %s' % (str(self.display), str(second.display)))
         assert(isinstance(second, CycGroupElem))
         try:
             assert(self.group_order == second.group_order)
@@ -100,7 +100,7 @@ class CycGroupElem(FinGroupElem):
             raise TypeError
         cyc_logger.debug('Elements are from cyclic group of group_order %d' % self.group_order)
         result = (self._number + second._number) % self.group_order
-        cyc_logger.debug('result is %s' %(str(result)))
+        cyc_logger.debug('result is %s' % (str(result)))
         if self.associated_group is None:
             return CycGroupElem(result, self.group_order)
         else:
